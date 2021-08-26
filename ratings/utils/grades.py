5f6f92920d0c2a0f04fa99da1f5194c06b27011c
vkameswaran <vaishantkameswaran@gmail.com>
2021-08-26 11:17:23 -0400

    Fix API calls

--- ratings/utils/grades.py
+++ ratings/utils/grades.py
@@ -8,25 +8,18 @@ def findInfo(courses):
             ret.append("Error: Invalid code %s" % course)
             continue
         code = course.strip().upper()
-        url = 'https://critique.gatech.edu/course.php?id=' + code.replace(" ", "%20")
+        url = "https://c4citk6s9k.execute-api.us-east-1.amazonaws.com/test/data/course?courseID=" + code.replace(" ", "%20")
 
         r = requests.get(url)
 
-        soup = BeautifulSoup(r.text, 'html.parser')
-
-        try:
-            mainTable = soup.findAll("table")[0].find("tbody").find("tr")
-            avgGpa = float(mainTable.findAll("td")[1].get_text())
-            percents = [int(mainTable.findAll("td")[i+2].get_text())
-                        for i in range(5)] # A B C D F
-        except:
-            ret.append("Error: No data available for %s" % code)
-            continue
-
-        if (percents[0] < 30):
-            ret.append("%s is a difficult class." % code)
-        elif (percents[0] < 50):
-            ret.append("%s is a moderate class." % code)
+        j = r.json()
+        if (j["header"][0]["full_name"]):
+            if (j["header"][0]["avg_a"] < 30):
+                ret.append("%s is a difficult class." % code)
+            elif (j["header"][0]["avg_a"] < 50):
+                ret.append("%s is a moderate class." % code)
+            else:
+                ret.append("%s is an easy class." % code)
         else:
-            ret.append("%s is an easy class." % code)
+            ret.append("Error: No data available for %s" % code)
     return ret
