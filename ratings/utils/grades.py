b8f1afefc2287e611209f0962c453b8e5e26fc54
Vaishant Kameswaran <vaishantkameswaran@gmail.com>
2022-05-12 16:27:25 +0800

    Update grades.py

--- ratings/utils/grades.py
+++ ratings/utils/grades.py
@@ -2,6 +2,8 @@ import requests
 from bs4 import BeautifulSoup
 import re
 
+mean = lambda l : sum(l)/len(l)
+
 def get_valid_courses(courses):
     matches = re.findall(r"([A-Za-z]+) ?([0-9]{4})", courses)
     return [match[0].upper() + " " + match[1] for match in matches]
@@ -9,16 +11,20 @@ def get_valid_courses(courses):
 def findInfo(courses):
     ret = []
     for course in get_valid_courses(courses):
-        url = "https://c4citk6s9k.execute-api.us-east-1.amazonaws.com/test/data/course?courseID=" + course.replace(" ", "%20")
+        url = "https://c4citk6s9k.execute-api.us-east-1.amazonaws.com/prod/data/course?courseID=" + course.replace(" ", "%20")
         r = requests.get(url)
         j = r.json()
+        average_a = mean([i["A"] for i in j["raw"]])
         if (j["header"][0]["full_name"]):
-            if (j["header"][0]["avg_a"] < 30):
+            if (average_a < 30):
                 ret.append("%s is a difficult class." % course)
-            elif (j["header"][0]["avg_a"] < 50):
+            elif (average_a < 50):
                 ret.append("%s is a moderate class." % course)
             else:
                 ret.append("%s is an easy class." % course)
         else:
             ret.append("Error: No data available for %s" % course)
     return ret
+
+if __name__ == "__main__":
+    findInfo("PSYC 1101")
