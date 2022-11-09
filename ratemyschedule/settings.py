f38c9a44368dceb741d0acbd211949f00ed5865e
vkameswaran <vaishantkameswaran@gmail.com>
2022-11-08 20:09:15 -0500

    Add custom domain as eligible host

--- ratemyschedule/settings.py
+++ ratemyschedule/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&k3q+pmu
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', '') != 'False'
 
-ALLOWED_HOSTS = ['.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['.herokuapp.com', 'localhost', '.vaishant.com']
 
 
 # Application definition
